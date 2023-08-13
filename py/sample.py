f97b99306382408fe24390881aa417d49000064d
mosunset <driveasano@gmail.com>
2023-08-14 05:38:30 +0900

    up

---
+++ py/sample.py
@@ -0,0 +1,20 @@
+# 文字装飾
+pen = {
+    "id": 0,
+    "b": 1,
+    "i": 1,
+    "u": 1,
+    "fc": 0xffffff,
+    "fo": 255,
+    "bc": 0xffffff,
+    "bo": 255,
+    "ec": 0xffffff,
+    "et": 4,
+    "fs": 7,
+    "fs": 0,
+    "rb": [1, 4, 5],
+    "of": 2,
+    "te": 2,
+    "hg": 1,
+}
+print(len(pen["rb"]))
