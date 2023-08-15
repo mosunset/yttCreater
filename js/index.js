281f64fb7d3bcde463dec3c5cb2b842e634e0038
mosunset <driveasano@gmail.com>
2023-08-15 16:57:07 +0900

    asedf

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log("Hello world!");
+
+const myPlayer = videojs('video1');
+myPlayer.ready(() => {
+
+
+    const currentVolume = myPlayer.volume();
+
+    // 音量を指定
+    myPlayer.volume(0.1);
+    // Playerを再生する
+    myPlayer.play();
+});
+
+function hidden() {
+    console.log("test");
+}
